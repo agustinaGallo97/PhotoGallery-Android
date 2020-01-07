9a59584af8071d69bb24079cfb3832f20b16ebc0
Agustina <33355750+agustinaGallo97@users.noreply.github.com>
2020-01-07 10:00:14 -0300

    Feature/chapter 25 (#2)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoAdapter.kt
@@ -1,25 +1,43 @@
 package com.bignerdranch.android.photogallery
 
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
+class PhotoAdapter(
+    private val galleryItems: List<GalleryItem>,
+    private val thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
+) :
   RecyclerView.Adapter<PhotoAdapter.PhotoHolder>() {
 
+  private var context: Context? = null
+
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
-    val textView = TextView(parent.context)
-    return PhotoHolder(textView)
+    val view = LayoutInflater
+      .from(parent.context)
+      .inflate(R.layout.list_item_gallery, parent, false) as ImageView
+    context = parent.context
+    return PhotoHolder(view)
   }
 
   override fun getItemCount(): Int = galleryItems.size
 
   override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
     val galleryItem = galleryItems[position]
-    holder.bindTitle(galleryItem.title)
+    val placeholder: Drawable = ContextCompat.getDrawable(
+      context!!,
+      R.drawable.bill_up_close
+    ) ?: ColorDrawable()
+    holder.bindDrawable(placeholder)
+    thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
   }
 
-  inner class PhotoHolder(itemTextView: TextView) : RecyclerView.ViewHolder(itemTextView) {
-    val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+  inner class PhotoHolder(itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView) {
+    val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
   }
 }
